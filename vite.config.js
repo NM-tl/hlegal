3879a489a5cb9895d3beb8e573cddd7e5b14cdf6
NM-tl <nikitam42015@gmail.com>
2024-02-13 10:22:25 +0200

    init pages routes
    - added pages
    - update build config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 
+const pages = ['about', 'contact-modal', 'contact', 'publication', 'publications', 'service', 'services', 'team','teammate'];
+
+const inputPages = Object.fromEntries(pages.map(page => [page, `./pages/${page}.html`]));
+
 export default defineConfig({
     base: './',
     build: {
         rollupOptions: {
-        input: {
-            main: './index.html',
-            pages: './pages/test.html',
-        },
+            input: {
+                main: './index.html',
+                ...inputPages,
+            },
         },
     },
 });
