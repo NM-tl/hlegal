d004596cb937f3465e5d87ca35962172c8df0b01
Nikita Maiboroda <30575825+NM-tl@users.noreply.github.com>
2024-03-01 21:39:47 +0200

    Dev (#6)
    * correct header / start about page
    
    * About page title+responsive  and start team photo
    
    * team text
    
    * Text section
    
    * added xs screen for mobile to config
    
    * added menu and arrow icon-google
    
    * added components to nav for vercel
    
    * team page mobile version 80% ready
    
    * custom screens added
    
    * test to fix bg-url
    
    * back (bg-url)
    
    * added hover to arrow
    
    * added screens/need to check together
    
    * added responsive team pages with no footer
    
    * fixed sizes of photos/grid responsive
    
    * added menu icon burger to footer
    
    * fixed styles 338/264
    
    * added 100% responsive for teammate page
    
    * added team active hover statement
    
    * added team active hover statement
    
    * init slider about page
    
    - init slider
    - make directory for future build sliders
    - fix section bg
    
    * init slider home
    
    - added slider for home in components page
    
    * footer
    
    * footer line
    
    * publications page
    
    * header link fix
    
    * init modal in components page
    
    - resolve conflicts
    - init modal
    - added loop at slider home
    
    * created store for clients, posts, services
    
    * fix footer svg hover
    
    * added footer on page
    
    * added footer on page
    
    * changed shadow
    
    * empty commit
    
    * 20% of page ready/mob
    
    * init modal menu at about page
    
    * full resonsive added
    
    * modal window added
    
    * fixed issue with grid HD
    
    * fixed issue wrapper HD
    
    * init publication page
    
    - add structure
    - init hovers
    - add responsive
    
    ---------
    
    Co-authored-by: Vladyslav-Markevych <vladen035@gmail.com>
    Co-authored-by: Daria Neshcheretnaya <dashkan1994@gmail.com>

---
+++ assets/store/posts.js
@@ -0,0 +1,38 @@
+export const posts = [
+    {
+        image: "",
+        title: "Equity associate",
+        date: "April 22",
+        short: "I'm a jun on projects. I am a co-owner of a law firm. How it happened. Before entering my parents told me that if I…"
+    },
+    {
+        image: "",
+        title: "Penalty for the Good Corporation: what will happen to Google and others for violating the GDPR",
+        date: "September 29",
+        short: "And why the recovery of 50 million euros could be a fateful precedent According to Mind.ua January 21 National Commission for Data Protection."
+    },
+    {
+        image: "",
+        title: "Millennial Testament",
+        date: "August 19",
+        short: "What and how modern vloggers, opinion leaders and crypto-millionaires will inherit A typical legacy of the Ukrainian generation X looks like this: an apartment in…"
+    },
+    {
+        image: "",
+        title: "Language Law: How to Make Your Website Relevant",
+        date: "August 19",
+        short: "Natalia Kyryk, head of content studio Wordfactory.ua First, it's beautiful! This is the number one argument in favor of the Ukrainian-language site. Secondly, it is effective. IN…"
+    },
+    {
+        image: "",
+        title: "Reimbursement of the child's tuition fees in kindergarten and school",
+        date: "February 5, 2018",
+        short: "What we are talking about In June this year, the Verkhovna Rada amended the Tax Code of Ukraine. From now on, parents who receive an official…"
+    },
+    {
+        image: "",
+        title: "Currency control is dead, long live currency control!",
+        date: "June 14, 2018",
+        short: `Experts hold conferences, round tables and other events on "liberalization of currency legislation." Lawyers-experts-novelists rub their fingers about "ctrl + c / ctrl + v".`
+    },
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,118 +1,214 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./components.html",
-    "./pages/**/*.html"
-  ],
+  content: ["./index.html", "./components.html", "./pages/**/*.html"],
   theme: {
-    fontWeight: {
-      normal: '400',
-      bold: '700'
+    screens: {
+      xs: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      sd: "1440px",
+      "2xl": "1536px",
+      hd: "1920px",
     },
 
-    extend: { 
+    fontWeight: {
+      normal: "400",
+      bold: "700",
+    },
+
+    extend: {
       fontFamily: {
-        'main': ['Montserrat', 'sans-serif'],
+        main: ["Montserrat", "sans-serif"],
       },
 
       colors: {
         /* main colors */
-        'main': '#323264',
-        'main-alt': '#46466E',
-        'main-soft': '#686887',
+        main: "#323264",
+        "main-alt": "#46466E",
+        "main-soft": "#686887",
 
         /* accent red color */
-        'accent-color': '#F0145A',
+        "accent-color": "#F0145A",
 
         /* active colors */
-        'active-regular': '#24A3FF',
-        'active-hover': '#0085FF',
-        'active-dark': '#006CCF',
+        "active-regular": "#24A3FF",
+        "active-hover": "#0085FF",
+        "active-dark": "#006CCF",
 
         /* grey colors */
-        'grey-bg': '#F5F5FA',
-        'grey-bg-alt': '#EDEDF7',
-        'grey-text': '#B3B3BA',
-        'grey-separator': '#E6E6F0',
+        "grey-bg": "#F5F5FA",
+        "grey-bg-alt": "#EDEDF7",
+        "grey-text": "#B3B3BA",
+        "grey-separator": "#E6E6F0",
+        "modal-close": "#EC6F5D",
       },
 
       boxShadow: {
-        'once': '0 5px 50px 5px rgba(36, 163, 255, 0.5)',
+        once: "0 5px 25px 5px rgba(36, 163, 255, 0.5)",
       },
 
       /* gradient colors */
 
       backgroundImage: (theme) => ({
-        'initial': 'linear-gradient(140deg, rgba(49,49,99,1) 0%, rgba(123,66,96,1) 100%)',
-        'soft': 'linear-gradient(93deg, rgba(52,50,99,1) 0%, rgba(123,66,95,1) 100%)',
-        'red': 'linear-gradient(172deg, rgba(104,49,99,1) 0%, rgba(177,33,94,1) 100%)',
-        'superSoft': 'linear-gradient(35deg, rgba(99,61,113,1) 0%, rgba(49,49,99,1) 100%)',
-        'redText': 'linear-gradient(90deg, rgba(240,19,89,1) 0%, rgba(239,128,128,1) 100%)',
+        initial:
+          "linear-gradient(140deg, rgba(49,49,99,1) 0%, rgba(123,66,96,1) 100%)",
+        soft: "linear-gradient(93deg, rgba(52,50,99,1) 0%, rgba(123,66,95,1) 100%)",
+        red: "linear-gradient(172deg, rgba(104,49,99,1) 0%, rgba(177,33,94,1) 100%)",
+        superSoft:
+          "linear-gradient(35deg, rgba(99,61,113,1) 0%, rgba(49,49,99,1) 100%)",
+        redText:
+          "linear-gradient(90deg, rgba(240,19,89,1) 0%, rgba(239,128,128,1) 100%)",
       }),
 
       fontFamily: {
-        'main': ['Montserrat'],
+        main: ["Montserrat"],
       },
 
       fontSize: {
-        'hero-h1': ['38px', {
-          lineHeight: '46px',
-          fontWeight: '700',
-        }],
+        "hero-h1": [
+          "38px",
+          {
+            lineHeight: "46px",
+            fontWeight: "700",
+          },
+        ],
 
-        'h1': ['36px', {
-          lineHeight: '44px',
-          fontWeight: '400',
-        }],
+        h1: [
+          "36px",
+          {
+            lineHeight: "44px",
+            fontWeight: "400",
+          },
+        ],
 
-        'h2': ['30px', {
-          lineHeight: '36px',
-          fontWeight: '700',
-        }],
+        h2: [
+          "30px",
+          {
+            lineHeight: "36px",
+            fontWeight: "700",
+          },
+        ],
 
-        'h3': ['14px', {
-          lineHeight: '17px',
-          fontWeight: '700',
-        }],
+        h3: [
+          "14px",
+          {
+            lineHeight: "17px",
+            fontWeight: "700",
+          },
+        ],
 
-        'h3-caps': ['22px', {
-          lineHeight: '27px',
-          fontWeight: '700',
-          textTransform: 'capitalize'
-        }],
-        
-        'h4': ['16px', {
-          lineHeight: '20px',
-          fontWeight: '700',
-        }],
+        "h3-caps": [
+          "22px",
+          {
+            lineHeight: "27px",
+            fontWeight: "700",
+            textTransform: "capitalize",
+          },
+        ],
 
-        'paragraph-font': ['16px', {
-          lineHeight: '26px',
-          fontWeight: '400',
-        }],
+        h4: [
+          "16px",
+          {
+            lineHeight: "20px",
+            fontWeight: "700",
+          },
+        ],
 
-        'list-font': ['18px', {
-          lineHeight: '26px',
-          fontWeight: '400',
-        }],
+        "paragraph-font": [
+          "16px",
+          {
+            lineHeight: "26px",
+            fontWeight: "400",
+          },
+        ],
+        "footer-font": [
+          "16px",
+          {
+            lineHeight: "20px",
+            fontWeight: "400",
+          },
+        ],
+        "paragraph-12": [
+          "12px",
+          {
+            lineHeight: "15px",
+            fontWeight: "700",
+            letterSpacing: "1px",
+          },
+        ],
+        "paragraph-14": [
+          "14px",
+          {
+            lineHeight: "22px",
+            fontWeight: "400",
+          },
+        ],
+        "paragraph-about": [
+          "19px",
+          {
+            lineHeight: "30px",
+            fontWeight: "400",
+          },
+        ],
+        "list-font": [
+          "18px",
+          {
+            lineHeight: "26px",
+            fontWeight: "400",
+          },
+        ],
 
-        'link-font': ['16px', {
-          lineHeight: '20px',
-          fontWeight: '400',
-        }],
+        "link-font": [
+          "16px",
+          {
+            lineHeight: "20px",
+            fontWeight: "400",
+          },
+        ],
+        "number-about": [
+          "36px",
+          {
+            lineHeight: "44px",
+            fontWeight: "700",
+          },
+        ],
+        "team-md": [
+          "46px",
+          {
+            lineHeight: "56px",
+            fontWeight: "700",
+          },
+        ],
+        "team-lg": [
+          "86px",
+          {
+            lineHeight: "105px",
+            fontWeight: "700",
+          },
+        ],
+        "team-xl": [
+          "96px",
+          {
+            lineHeight: "117px",
+            fontWeight: "700",
+          },
+        ],
       },
     },
 
     /* Wrappers */
     maxWidth: {
-      'desc-wrapper': '1440px',
-      'tab-wrapper': '768px',
-      'mob-wrapper': '375px',
+      "desc-wrapper": "1440px",
+      "lg": "1280px",
+      "tab-wrapper": "768px",
+      "mob-wrapper": "375px",
+      "publications-xs": "400px",
+      "publications-md": "648px",
+      "publications-lg": "1600px",
     },
 
-  plugins: [],
-
-  }
-}
+    plugins: [],
+  },
+};
