6db351e24312e4701c3d27cdab9e062287aadb5d
NM-tl <nikitam42015@gmail.com>
2024-03-05 20:56:57 +0200

    try render services

--- assets/store/services.js
+++ assets/store/services.js
@@ -1,7 +1,7 @@
-export const services = [
+const services = [
     {
         title: "IT and e-commerce",
-        icon: "icon-Ecommerce",
+        icon: "Ecommerce",
         text: "An endless stream of contracts, tax issues, labor relations, corporate papers - legal work distracts you from doing business. But there is not much work to do to hire an in-house lawyer.",
         additional: "An endless stream of contracts, tax issues, labor relations, corporate papers - legal work distracts you from doing business. But there is not much work to do to hire an in-house lawyer. In addition, over time, the work will become the same type and uninteresting for a full-time lawyer. And we know very well how difficult it is to find a good candidate with experience in the field of information technology in Ukraine.",
         area: [
@@ -17,7 +17,7 @@ export const services = [
     },
     {
         title: "Comprehensive daily legal support",
-        icon: "icon-Support",
+        icon: "Support",
         text: "In addition, over time, the work will become the same type and uninteresting for a full-time lawyer. And we know very well how difficult it is to find a good candidate with experience in the field of information technology in Ukraine.",
         additional: "An endless stream of contracts, tax issues, labor relations, corporate papers - legal work distracts you from doing business. But there is not much work to do to hire an in-house lawyer. In addition, over time, the work will become the same type and uninteresting for a full-time lawyer. And we know very well how difficult it is to find a good candidate with experience in the field of information technology in Ukraine.",
         area: [
@@ -33,7 +33,7 @@ export const services = [
     },
     {
         title: "Structuring corporate relationships",
-        icon: "icon-Coprorate",
+        icon: "Coprorate",
         text: "Instead of just providing legal services, we will hire a lawyer or a whole team that will fit into the daily life of your company.",
         additional: "An endless stream of contracts, tax issues, labor relations, corporate papers - legal work distracts you from doing business. But there is not much work to do to hire an in-house lawyer. In addition, over time, the work will become the same type and uninteresting for a full-time lawyer. And we know very well how difficult it is to find a good candidate with experience in the field of information technology in Ukraine.",
         area: [
@@ -49,7 +49,7 @@ export const services = [
     },
     {
         title: "Building contractual relations",
-        icon: "icon-Trust",
+        icon: "Trust",
         text: "Once a week, or week after month, you have your own lawyer by your side. Anyone in the company can catch it at the coffee maker and just talk about current affairs instead of waiting for update status.",
         additional: "And all the other time he will work on new diverse projects, so he will definitely not stop in professional development. One way or another, all Axon Partners will work for you instead of a corporate lawyer.",
         area: [
@@ -65,7 +65,7 @@ export const services = [
     },
     {
         title: "Protection of business, intellectual property",
-        icon: "icon-Protection",
+        icon: "Protection",
         text: "And all the other time he will work on new diverse projects, so he will definitely not stop in professional development. One way or another, all Axon Partners will work for you instead of a corporate lawyer.",
         additional: "An endless stream of contracts, tax issues, labor relations, corporate papers - legal work distracts you from doing business. But there is not much work to do to hire an in-house lawyer. In addition, over time, the work will become the same type and uninteresting for a full-time lawyer. And we know very well how difficult it is to find a good candidate with experience in the field of information technology in Ukraine.",
         area: [
@@ -81,7 +81,7 @@ export const services = [
     },
     {
         title: "Judicial representation",
-        icon: "icon-Court",
+        icon: "Court",
         text: "Is it a small, medium-sized company or a market leader? Local business or multinational company? Do you have your own legal department or not?",
         additional: "An endless stream of contracts, tax issues, labor relations, corporate papers - legal work distracts you from doing business. But there is not much work to do to hire an in-house lawyer. In addition, over time, the work will become the same type and uninteresting for a full-time lawyer. And we know very well how difficult it is to find a good candidate with experience in the field of information technology in Ukraine.",
         area: [
@@ -96,3 +96,20 @@ export const services = [
         ],
     },
 ]
+
+document.addEventListener("DOMContentLoaded", () => {
+    const section = document.getElementById("services");
+
+    services.forEach(service => {
+        const { title, icon, text } = service;
+        section.innerHTML += `
+        <div class="flex flex-col gap-[15px]">
+            <div class="flex justify-center items-center w-[92px] h-[92px] bg-white rounded-[24px] mb-[15px]">
+                <img src="../images/services/serv/${icon}.svg" alt="${title}">
+            </div>
+            <p class="text-h3-caps text-active-regular hover:text-accent-color">${title}</p>
+            <p class="text-paragraph-font text-white">${text}</p>
+        </div>    
+        `;
+    });
+});
